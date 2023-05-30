a874cda2cd4d26cd69f0b0352cb057e53fd028af
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-05-30 22:00:36 +0530

    this dependence on position and how it is called

--- lec2/index.js
+++ lec2/index.js
@@ -1,1 +1,31 @@
+// 1.
+console.log("1. this in normal function and arrow function");
 console.log(this);
+
+const obj = {
+    fn: function(){
+        console.log(this);
+    },
+    fn2: () => {
+        console.log(this);
+    }
+}
+obj.fn(); //obj
+obj.fn2(); //window
+
+// 2.
+console.log("2. this");
+
+function x(){
+    console.log(this);
+    function y(){
+        console.log(this)
+        function z(){
+            console.log(this);
+        }
+        z();
+    }
+    y(); 
+}
+
+x();
