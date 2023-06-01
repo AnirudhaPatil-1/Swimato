41b21b0920e0c64a6bb6d4112190efd1592be939
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-06-01 18:25:27 +0530

    bundler - PARCEL - functionalities - details

--- namasteReact/App.js
+++ namasteReact/App.js
@@ -1,3 +1,31 @@
+/*
+*Created a server
+*HMR - hot module replacement
+*file watcher algorithm - c++
+*bundling
+*minify
+*cleaning our code
+*dev and production builds
+*super fast build algorithm
+*image  optimization 
+*caching while development
+*compression
+*compatible with older version of Browser
+*HTTPS on dev
+*port number
+*consistent hashing algorithm
+*zero config
+*tree shaking
+*
+*/
+
+import { config } from "process"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Browser } from "selenium-webdriver"
+import { buildPath } from "selenium-webdriver/http"
+import { CommandExecutor, Server } from "selenium-webdriver/safari";
+
 const heading1 = React.createElement("h1", {
     id: "heading1"
 }, "HEADING 1");
