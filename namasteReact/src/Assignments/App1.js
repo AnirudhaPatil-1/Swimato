2298cdab12165f28000b6f495b5f0ff4e0e7cfff
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-06-12 23:41:58 +0530

    UseEffect - hook - assignment

---
+++ namasteReact/src/Assignments/App1.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {UseEffect1} from "../Assingments/UseEffect";
+
+
+const assignment = () => {
+    return (
+        <h1 className="assignment-heading"></h1>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<assignment/>);
