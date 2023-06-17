4011051faefad9f99b77d059a322001dfe5daf6e
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-06-17 21:56:02 +0530

    nested routing - implemented using router

--- namasteReact/src/App.js
+++ namasteReact/src/App.js
@@ -1,20 +1,53 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './components/Header.js';
+import Body from './components/Body.js';
+import Footer from './components/Footer.js';
+import {IMG_CDN_URL} from './components/constants.js';
+import {createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
+import Error from './components/Error.js'
+import About from './components/About.js'
+import Home from './components/Home.js'
+import Contact from './components/Contact.js'
 
-import * as MainHeader from "./components/Header"; /* Imported using import * as namespace  */ 
-import Body from "./components/Body"; /* Imported using default export */
-import { Footer as MainFooter } from "./components/Footer"; /* Imported using Named Import Map */
 
 const AppLayout = () => {
-  return (
-    <>
-      <MainHeader.Header />
-      <Body />
-      <MainFooter />
-    </>
-  );
-};
+    return (
+        <>
+            <Header/>   
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+}
+
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element: <AppLayout/>,
+        errorElement: <ErrorElement/>,
+        children: [
+            {
+                path: '/body',
+                element: <Body/>
+            },
+            {
+                path: '/about',
+                element: <About/>
+            },
+            {
+                path: '/home',
+                element: <Home/>
+            },
+            {
+                path: '/contact',
+                element: <Contact/>
+            },
+            
+        ]
+        },
+    
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<AppLayout />);
+root.render(<RouterProvider router = {appRouter}/>);
