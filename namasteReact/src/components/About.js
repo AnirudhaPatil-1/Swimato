70083a6798b716e3024f3cfb137d880f80a153c5
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-06-17 21:46:36 +0530

    Header - About component

---
+++ namasteReact/src/components/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <>
+        <h1> About us Page</h1>
+        <p>
+            This is the Namaste React Course
+        </p>
+        </>
+    )
+}
+
+export default About;
