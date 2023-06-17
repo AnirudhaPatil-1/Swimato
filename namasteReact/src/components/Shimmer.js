7ae8e851ef16afb7b72b879e2be1b1ed4ea08e1d
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-06-17 21:52:34 +0530

    shimmer effect and element using npm package

--- namasteReact/src/components/Shimmer.js
+++ namasteReact/src/components/Shimmer.js
@@ -1,24 +1,13 @@
-import { SHIMMER_RES_CARDS_COUNT } from '../config';
+import React from 'react'
+import { Image, Shimmer } from 'react-shimmer'
 
-const CardShimmer = () => {
+function App() {
   return (
-    <div className="shimmer-card">
-      <div className="shimmer-img stroke animate"></div>
-      <div className="shimmer-title stroke animate"></div>
-      <div className="shimmer-tags stroke animate "></div>
-      <div className="shimmer-details stroke animate "></div>
+    <div>
+      <Image
+        src='https://source.unsplash.com/random/800x600'
+        fallback={<Shimmer width={800} height={600} />}
+      />
     </div>
-  );
-}
-
-const Shimmer = () => {
-  return (
-    <div className="shimmer-container">
-      {Array.from({length:SHIMMER_RES_CARDS_COUNT}).map((element, index) => {
-        return <CardShimmer key ={index} />
-      }) }
-    </div>   
   )
 }
-
-export default Shimmer;
