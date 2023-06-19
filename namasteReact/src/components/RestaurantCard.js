3abce02bb5dae5508e9803cf3a53e1b115d753b4
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-06-19 15:14:12 +0530

    update - path for the cdn links updated

--- namasteReact/src/components/RestaurantCard.js
+++ namasteReact/src/components/RestaurantCard.js
@@ -1,15 +1,19 @@
-import {IMG_CDN_URL} from './constants.js';
+import { IMG_CDN_URL } from "../constants";
 
-const RestaurantCard = ({name, cuisines, cloudinaryImageId, avgRating, lastMileTravelString}) =>{
-    return (
-        <div className="card">
-            <img src={IMG_CDN_URL + cloudinaryImageId}/>
-            <span className="card-title">{name} </span>
-            <span className="card-tags">{cuisines.join(", ")} </span>
-            <span className="card-rating">{avgRating} </span>
-            <h4>{lastMileTravelString} minutes</h4>
-        </div>
-    )
-}
+const RestaurantCard = ({
+  name,
+  cuisines,
+  cloudinaryImageId,
+  lastMileTravelString,
+}) => {
+  return (
+    <div className="card">
+      <img src={IMG_CDN_URL + cloudinaryImageId} />
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h4>{lastMileTravelString} minutes</h4>
+    </div>
+  );
+};
 
 export default RestaurantCard;
