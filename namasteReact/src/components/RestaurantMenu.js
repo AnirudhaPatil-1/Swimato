86bdeaf97c7346b74c3737ffb0062640a05119ba
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-06-19 15:15:44 +0530

    feature - dynamic routing - make the hard coded menu list into dynamic menu list using

--- namasteReact/src/components/RestaurantMenu.js
+++ namasteReact/src/components/RestaurantMenu.js
@@ -1,31 +1,51 @@
-import { Params } from "react-router-dom"
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IMG_CDN_URL } from "../constants";
+import Shimmer from "./npmShimmer";
 
+const RestaurantMenu = () => {
+  // how to read a dynamic URL params
+  const { resId } = useParams();
+  // Use proper names
+  const [restaurant, setRestaurant] = useState(null);
 
+  useEffect(() => {
+    getRestaurantInfo();
+  }, []);
 
-const RestaurantMenu =  () => {
-    const id = useParams(); 
-    const {restaurantId} = id;
-    const [restaurant, setRestaurant] = useState({});
+  async function getRestaurantInfo() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/v4/full?lat=12.9351929&lng=77.62448069999999&menuId=" +
+        resId
+    );
+    const json = await data.json();
+    console.log(json.data);
+    setRestaurant(json.data);
+  }
 
-    useEffect(() => {
-        getRestaurantMenu()
-    }, []);
+  return !restaurant ? (
+    <Shimmer />
+  ) : (
+    <div className="menu">
+      <div>
+        <h1>Restraunt id: {resId}</h1>
+        <h2>{restaurant?.name}</h2>
+        <img src={IMG_CDN_URL + restaurant?.cloudinaryImageId} />
+        <h3>{restaurant?.area}</h3>
+        <h3>{restaurant?.city}</h3>
+        <h3>{restaurant?.avgRating} stars</h3>
+        <h3>{restaurant?.costForTwoMsg}</h3>
+      </div>
+      <div>
+        <h1>Menu</h1>
+        <ul>
+          {Object.values(restaurant?.menu?.items).map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-    const getRestaurantMenuItem =  async() => {
-        const data = await fetch("");
-        const json = data.json();
-        console.log(json);
-    
-    }
-
-    return (
-        <div>
-            <h1>Restaurant id: </h1>
-            <h2>{restaurant.name}</h2>
-        </div>
-    )
-}
-
-
-
+export default RestaurantMenu;
