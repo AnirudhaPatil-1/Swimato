68d2f20a194ed44bf30dfee7f7f29138695a6104
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-06-28 23:24:05 +0530

    update - add comments as notes to file

--- namasteReact/src/components/About.js
+++ namasteReact/src/components/About.js
@@ -1,19 +1,48 @@
-import ProfileFunctionalComponent from "./Profile";
-import ProfileClass from "./ProfileClass"
+import { Outlet } from "react-router-dom";
+import ProfileFunctionalComponet from "./Profile";
+import Profile from "./ProfileClass";
+import { Component } from "react";
 
-const About = () => {
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    //console.log("Parent - constructor");
+  }
+  componentDidMount() {
+    // Best place to make an Api call
+    //console.log("Parent - componentDidMount");
+  }
+  render() {
+    //console.log("Parent - render");
     return (
       <div>
         <h1>About Us Page</h1>
         <p>
-          {" "}
           This is the Namaste React Live Course Chapter 07 - Finding the Path 🚀
         </p>
-        <ProfileClass key="value" />
-        <ProfileFunctionalComponent key1 = "value1"/>
+        <ProfileFunctionalComponet />
       </div>
     );
-  };
-  
+  }
+}
+
 export default About;
-  
+
+/**
+ *
+ * Parent Constructor
+ * Parent render
+ *    First Child constructor
+ *    First Child render
+ *    Second Child constructor
+ *    Second Child render
+ *
+ *    DOM UPDATED for children
+ *
+ *    first Child componentDidMount
+ *    Second Child componentDid
+ *  Parent componentDidMount
+ *
+ *
+ */
