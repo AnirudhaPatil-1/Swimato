dcd6d140deab72314bc4ac494071e7f39d2eb570
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-06-30 23:31:36 +0530

    feature - useContext

---
+++ namasteReact/src/utils/useContext.js
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+const UserContext = createContext({
+    user:{
+        name: "Dummy User",
+        email: "dummy@gmail.com"
+    }
+});
+
+export default UserContext;
