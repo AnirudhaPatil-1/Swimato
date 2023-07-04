87f1bd849204ad37100d87075c3e47a294e4c5f9
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-07-05 01:08:49 +0530

    packages installed and styling files updated

--- namasteReact/tailwind.config.js
+++ namasteReact/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    extend: {
+			fontFamily: {
+				sans: ["Inter var"],
+			},
+		},
   },
   plugins: [],
 };
