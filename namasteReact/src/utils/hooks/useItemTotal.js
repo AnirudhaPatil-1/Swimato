23f86863b7019c8d561ab1fafa0d7c1e5d80ae10
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-07-06 23:31:28 +0530

    feature - custom hook to get item total in cart

---
+++ namasteReact/src/utils/hooks/useItemTotal.js
@@ -0,0 +1,19 @@
+import {userSelector} from "react-redux";
+
+import React from "react";
+
+function userItemTotal(){
+    const cartItems = useSelector((store) => store.cart.items);
+
+    const getItemTotal = () => {
+        let total = cartItems &&
+                    cartItems.map((item) => (item.price/ 100) *item.itemQuantity)
+                    .reduce((acc, curr) => acc + curr, 0 );
+
+        return total - 45;
+    };
+
+    return getItemTotal;
+}
+
+export default userItemTotal;
