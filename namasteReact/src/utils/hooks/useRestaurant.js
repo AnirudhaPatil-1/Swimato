f7db3cb04c40bfacb7124272d7d186f1d50a2000
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-07-08 23:06:12 +0530

    update - CORS issue resolved for menu items of restaurant

--- namasteReact/src/utils/hooks/useRestaurant.js
+++ namasteReact/src/utils/hooks/useRestaurant.js
@@ -9,7 +9,14 @@ const useRestaurant = (id) => {
 
 	const getRestaurantmenu = async () => {
 		const response = await fetch(
-			`https://www.swiggy.com/dapi/menu/v4/full?lat=28.3667854&lng=77.06939799999999&menuId=${id}`
+			//ME TRYING
+			`https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.591945&lng=73.73897649999999&restaurantId=${id}`
+			// `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.485132&lng=73.8110947&restaurantId=${id}&submitAction=ENTER`
+			//21395
+			// `https://www.swiggy.com/dapi/menu/v4/full?lat=12.9351929&lng=77.62448069999999&menuId=${id}`
+
+			//original
+			// `https://www.swiggy.com/dapi/menu/v4/full?lat=28.3667854&lng=77.06939799999999&menuId=${id}`
 		);
 
 		const json = await response.json();
