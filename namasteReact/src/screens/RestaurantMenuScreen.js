b81a30870efd02d7cdebcbb8427c99cc20c221a8
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-07-08 23:03:47 +0530

    update - fetching data and implementing it in menu items, styling layout

--- namasteReact/src/screens/RestaurantMenuScreen.js
+++ namasteReact/src/screens/RestaurantMenuScreen.js
@@ -2,6 +2,11 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+//@
+import { IMG_CDN_URL } from "../config";
+import { FOODITEM_IMG_URL } from "../config";
+
+
 // Styles
 import "./style.css";
 import "../theme/CommonStyle.css";
@@ -43,6 +48,14 @@ const RestaurantMenuScreen = () => {
 
 	const restaurant= useRestaurant(id);
 
+	//@ start
+	// const itemCards = restaurant?.cards[2].groupedCard.cardGroupMap.REGULAR.cards;
+	const itemCards = restaurant?.cards[2].groupedCard?.cardGroupMap?.REGULAR.cards[2].card.card.itemCards;
+  	console.log(itemCards);
+  	const basicInfo = restaurant?.cards[0]?.card?.card?.info;
+	const discountInfo = basicInfo?.aggregatedDiscountInfo?.descriptionList;
+	//@ end
+
 	const dispatch = useDispatch();
 
 	const handleAdd = (item) => {
@@ -50,18 +63,20 @@ const RestaurantMenuScreen = () => {
 	};
 
 	//get unique meal categories
-	function getUniqueMealCategories(){
-		let showUniqueCategories = [];
+	// function getUniqueCategories(){
+	// 	let showUniqueCategories = [];
 
-		let array = Object.values(restaurant?.menu.items);
+	// 	// let array = Object.values(restaurant?.menu.items);
+	// 	//@
+	// 	const array = Object.values(basicInfo);
 
-		array.map((item, restaurant) => {
-			showUniqueCategories.indexOf(item.category) === -1 &&
-				showUniqueCategories.push(item.category);
-		});
+	// 	array.map((item, restaurant) => {
+	// 		showUniqueCategories.indexOf(item.category) === -1 &&
+	// 			showUniqueCategories.push(item.category);
+	// 	});
 
-		return showUniqueCategories;
-	}
+	// 	return showUniqueCategories;
+	// }
 
 	//return
 	return (
@@ -70,37 +85,48 @@ const RestaurantMenuScreen = () => {
 				<div className="restaurantDetailscontainer">
 					<div>
 						<img
-							src={URL + restaurant.cloudinaryImageId}
+							// src={URL + restaurant.cloudinaryImageId}
+							src={IMG_CDN_URL + basicInfo.cloudinaryImageId}
 							className="restaurantDetailsImage"
 						/>
 					</div>
 
 					<div className="restaurantDetails">
 						<h2 className="color_light fw_thin font_size_8xl">
-							{restaurant.name}
+							{/* {restaurant.name} */}
+							{basicInfo.name}
 						</h2>
 						<p className="m_t_16 m_b_16 color_light_secondary">
-							{restaurant.cuisines.join(", ")}
+							{/* {restaurant.cuisines.join(", ")} */}
+							
+							{basicInfo.cuisines.join(", ")}
 						</p>
 						<p className="color_light_secondary font_size_large">
-							{restaurant.locality + ", " + restaurant.area}
+							{/* {restaurant.locality + ", " + restaurant.area} */}
+							{basicInfo.areaName}, {basicInfo.sla.lastMileTravel} km
+
+							{/* {basicInfo.city} / {basicInfo.areaName} / {basicInfo.name}{" "} */}
 						</p>
 
 						<div className="restaurantDetailsFooter">
 							<div>
 								<IconWithRightLabel
-									text={restaurant.avgRating}
+									// text={restaurant.avgRating}
+									text={basicInfo.avgRating}
 									textColor="#ffffff"
 									icon={<FaStar color="#ffffff" />}
 								/>
 								<p className="color_light_secondary m_t_6 font_size_small">
-									{restaurant.totalRatingsString}
+									{/* {restaurant.totalRatingsString} */}
+									{basicInfo.totalRatingsString}
+									
 								</p>
 							</div>
 
 							<div className="verticalSeparator"></div>
 							<div>
-								<p className="color_light">{restaurant.sla.slaString}</p>
+								{/* <p className="color_light">{restaurant.sla.slaString}</p> */}
+								<p className="color_light">{basicInfo.sla.slaString}</p>
 								<p className="color_light_secondary font_size_small m_t_6 fw_thin">
 									Delivery Time
 								</p>
@@ -110,10 +136,12 @@ const RestaurantMenuScreen = () => {
 
 							<div>
 								<p className="color_light">
-									{"₹ " + restaurant.costForTwo / 100}
+									{/* {"₹ " + restaurant.costForTwo / 100} */}
+									{"₹ " + basicInfo.costForTwo / 100}
 								</p>
 								<p className="color_light_secondary font_size_small m_t_6 fw_thin">
-									{restaurant.costForTwoMsg}
+									{/* {restaurant.costForTwoMsg} */}
+									{basicInfo.costForTwoMessage}
 								</p>
 							</div>
 						</div>
@@ -128,7 +156,8 @@ const RestaurantMenuScreen = () => {
 								/>
 							}
 							textColor="#ffffff"
-							text={restaurant.aggregatedDiscountInfo.descriptionList[0].meta}
+							// text={restaurant.aggregatedDiscountInfo.descriptionList[0].meta}
+							text={basicInfo.aggregatedDiscountInfo.descriptionList[0].meta}
 						/>
 
 						<div className="m_t_16">
@@ -140,7 +169,8 @@ const RestaurantMenuScreen = () => {
 									/>
 								}
 								textColor="#ffffff"
-								text={restaurant.aggregatedDiscountInfo.descriptionList[1].meta}
+								// text={restaurant.aggregatedDiscountInfo.descriptionList[1].meta}
+								text={basicInfo.aggregatedDiscountInfo.descriptionList[1].meta}
 							/>
 						</div>
 					</div>
@@ -148,21 +178,27 @@ const RestaurantMenuScreen = () => {
 				<div className="recommendedSectionContainer">
 					<div className="recommendedListContainer">
 						{/* <li>{getUniqueCategories()}</li> */}
-						<p>All Day breakfast</p>
-						<p>Winter Speacial</p>
-						<p>Pickle Mania</p>
-						<p>Bakery Snakes</p>
-						<p>Chaat</p>
-						<p>Channa Bhatura</p>
-						<p>Kulcha</p>
-						<p>chowmein</p>
+						{/* <h2 className="font_size_xl ">Cuisines</h2> */}
+						<div className="font_size_medium color_light_secondary_dark">
+						<h2 className="font_size_xl ">Cuisines</h2>
+						<div className="horizontalSeparator"></div>
+						<p></p>
+						<p> Indian </p>
+						<p>South Indian</p>
+						<p>American</p>
+						<p>Punjabi</p>
+						<p>Chinese</p>
+						<p>North Indian</p>
+						<p>Mexican</p>
+						</div>
 					</div>
-
 					<div className="recommendedItemsContainer">
 						<div className="recommendedItemsHeader">
 							<h2 className="font_size_xl">Recommended</h2>
 							<p className="font_size_small color_light_secondary_dark">
-								{Object.values(restaurant?.menu.items).length} ITEMS
+								{/* {Object.values(restaurant?.menu.items).length} ITEMS */}
+								{/* {Object.values(itemCards?.menu.items).length} ITEMS */}
+								{itemCards.length} ITEMS
 							</p>
 						</div>
 
@@ -174,8 +210,10 @@ const RestaurantMenuScreen = () => {
 							}}
 						>
 							<p>
-								{Object.values(restaurant?.menu.items).map((item) => (
+								{/* {Object.values(restaurant?.menu.items).map((item) => ( */}
+								{itemCards.map((item) => (
 									<>
+									{/* const itemInfo = item.card.info; */}
 										<div
 											style={{
 												display: "flex",
@@ -185,31 +223,32 @@ const RestaurantMenuScreen = () => {
 											}}
 										>
 											<div>
-												{item.isVeg ? (
+												{item.card.info.isVeg ? (
 													<img
 														src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Veg_symbol.svg/1200px-Veg_symbol.svg.png"
 														style={{ height: "20px", width: "20px" }}
 													/>
 												) : (
 													<img
-														src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Non_veg_symbol.svg/2048px-Non_veg_symbol.svg.png
-														"
+														src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Non_veg_symbol.svg/2048px-Non_veg_symbol.svg.png"
 														style={{ height: "20px", width: "20px" }}
 													/>
 												)}
 
-												<p className="fw_medium font_size_xl">{item.name}</p>
+												{/* <p className="fw_medium font_size_xl">{item.name}</p> */}
+												<p className="fw_medium font_size_xl font hover: font-bold">{item.card.info.name}</p>
 												<p className="fw_medium font_size_regular">
-													{"₹ " + item.price / 100}
+													{"₹ " + item.card.info.price / 100}
 												</p>
 
 												<p className="itemDescription">
-													{item.description ? item.description : "--"}
+													{item.card.info.description ? item.card.info.description : "--"}
 												</p>
 											</div>
 
 											<div className="recommendedItemImageContainer">
-												<img src={URL + item.cloudinaryImageId} />
+												{/* <img src={URL + item.card.info.imageId} /> */}
+												<img src={FOODITEM_IMG_URL + item.card.info.imageId} />
 												<button
 													className="addCtaCart"
 													onClick={()=> {
@@ -246,7 +285,7 @@ const RestaurantMenuScreen = () => {
 									return (
 										<div className="">
 											<div className="flex items-center mt-2 justify-between">
-												{item.isVeg ? (
+												{item.card.info.isVeg ? (
 													<img
 														src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Veg_symbol.svg/1200px-Veg_symbol.svg.png"
 														style={{
@@ -266,15 +305,14 @@ const RestaurantMenuScreen = () => {
 														}}
 													/>
 												)}
-
-											<p className=" w-36 text-sm">{item.name}</p>
+											<p className=" w-36 text-sm">{item.card.info.name}</p>
 											<div className="px-5">
 												<Cta item={item} />
 											</div>
 
 						
 											<p className="font-thin text-sm">
-												{"₹ " + (item.price / 100) * item.itemQuantity}
+												{"₹ " + (item.card.info.price / 100) * item.card.info.itemQuantity}
 											</p>
 										</div>
 									</div>
